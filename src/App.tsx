82750b009ae876d660830b4d53d7bc87fc83d177
hmarch <hmarch@cs.washington.edu>
2022-08-23 13:47:08 -0700

    Fix google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ReactGA from 'react-ga';
 
 const TRACKING_ID = "G-E61RJ18FS5"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const date = new Date();
 const time = date.getTime() - START_DATE.getTime();
